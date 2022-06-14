f66b035d98749ff1bab93b6a2fd89fdf5e52b1e9
virgiawankusuma <virgiawan150@gmail.com>
2022-06-14 22:51:44 +0700

    Create Contact List component

---
+++ src/ContactList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ContactItem from './ContactItem';
+
+const ContactList = ({ contacts }) => {
+  return (
+    <div className="contact-list">
+      {
+        contacts.map(contact => ( // map through the contacts array
+          <ContactItem key={contact.id} {...contact} /> // spread the contact object into the ContactItem component
+        ))
+      }
+    </div>
+  );
+};
+
