66bd280287ad18b99b6b13106dc814da54130279
virgiawankusuma <virgiawan150@gmail.com>
2022-06-14 22:43:02 +0700

    Create Contact Item Body component

---
+++ src/ContactItemBody.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactItemBody = ({ name, tag }) => {
+  return (
+    <div className="contact-item__body">
+      <div className="contact-item__name">{name}</div>
+      <div className="contact-item__username">@{tag}</div>
+    </div>
+  );
+};
+
+export default ContactItemBody;
