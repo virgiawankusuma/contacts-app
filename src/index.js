196ef33fcd44e56dd945b945abe4891ce976ab99
virgiawankusuma <virgiawan150@gmail.com>
2022-06-14 23:03:59 +0700

    treat CSS files as modules in JavaScript files

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ContactApp from './ContactApp';
 
+// styling
+import './style.css';
+
 const root = createRoot(document.getElementById('root'));
 root.render(<ContactApp/>);
