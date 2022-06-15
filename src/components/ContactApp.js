3ff1a57f796e2fe94421a8bd4338b4300ad0e6f4
virgiawankusuma <virgiawan150@gmail.com>
2022-06-15 22:31:02 +0700

    change Contact App Component into Class Component

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -2,15 +2,30 @@ import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
 
-const ContactApp = () => {
-  const contacts = getData();
+class ContactApp extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      contacts: getData(),
+    }
 
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} />
-    </div>
-  );
+    this.onDeleteHandle = this.onDeleteHandle.bind(this);
+  };
+  
+  onDeleteHandle (id) {
+    const contacts = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts });
+  }
+
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandle}/>
+      </div>
+    );
+  }
+
 };
 
 export default ContactApp;
