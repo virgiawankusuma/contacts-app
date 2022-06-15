7bc65fc2bf111535923edc7384ef05427a648feb
virgiawankusuma <virgiawan150@gmail.com>
2022-06-15 22:24:54 +0700

    provide id and onDelete property on Contact List Component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import ContactItem from './ContactItem';
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, onDelete }) => {
   return (
     <div className="contact-list">
       {
         contacts.map(contact => ( // map through the contacts array
-          <ContactItem key={contact.id} {...contact} /> // spread the contact object into the ContactItem component
+          <ContactItem 
+          key={contact.id}
+          id={contact.id}
+          onDelete={onDelete}
+          {...contact} /> // spread the contact object into the ContactItem component
         ))
       }
     </div>
