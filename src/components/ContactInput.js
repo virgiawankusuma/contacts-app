ad0e250b9a2eb833de07905610124834762560be
virgiawankusuma <virgiawan150@gmail.com>
2022-06-16 20:25:12 +0700

    create Contact Input Controlled Component

--- src/components/ContactInput.js
+++ src/components/ContactInput.js
@@ -1,7 +1,43 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 
 class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+      this.state = {
+        name: '',
+        tag: '',
+      }
+
+      this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+      this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+      this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+
+    }
+    
+    onNameChangeEventHandler(event) {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          name: event.target.value,
+        }
+      });
+    }
+
+    onTagChangeEventHandler(event) {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          tag: event.target.value,
+        }
+      });
+    }
+    
+    onSubmitEventHandler(event) {
+      event.preventDefault();
+      this.props.addContact(this.state);
+    }
+    
   render() {
     return (
       <form className="contact-input">
